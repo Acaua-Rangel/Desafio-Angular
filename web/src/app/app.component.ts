cfbba52c8bbd37bcba19c3514c0462b0114bb720
AcauRangel <acauarangel17@gmail.com>
2025-05-01 20:43:35 -0300

    Login page

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginFormComponent } from './components/login-form/login-form.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginFormComponent],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ web/src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RightLoginComponent } from '../right-login/right-login.component';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [RightLoginComponent],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+
+}
