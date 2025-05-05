5839db827bca53e9590ec561f979a420b3fc3f04
AcauRangel <acauarangel17@gmail.com>
2025-05-05 11:27:24 -0300

    Auth Request

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPageComponent],
+  imports: [LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- web/src/app/components/login-form/login-form.component.ts
+++ web/src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../../dto/user.dto';
 
 @Component({
   selector: 'app-login-form',
@@ -6,6 +8,57 @@ import { Component } from '@angular/core';
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    console.log(this.userIsSaved());
+  }
+
+  messageError(show: boolean): void {
+    const messsageError = document.getElementById('message-error') as HTMLElement;
+    messsageError.style.display = `${show ? 'block' : 'none'}`;
+  };
+
+  saveUser(user: User): void {
+    localStorage.setItem("id", user.id.toString());
+    localStorage.setItem("nome", user.nome);
+    localStorage.setItem("email", user.email);
+  }
+
+  userIsSaved(): boolean {
+    const id = localStorage.getItem("id");
+    const nome = localStorage.getItem("nome");
+    const email = localStorage.getItem("email");
+
+    if (id && nome && email) {
+      return true;
+    }
+    return false;
+  }
+
+  submit(event: Event): void {
+    event.preventDefault();
+
+    const login = (<HTMLInputElement>document.getElementById('login')).value;
+    const password = (<HTMLInputElement>document.getElementById('password')).value;
+
+    const body = {
+      nome: login,
+      senha: password,
+    };
+
+    this.http.post('http://localhost:3001/login', body, {
+      headers: new HttpHeaders({ 'Accept': 'application/json' }),
+      withCredentials: false
+    }).subscribe({
+      next: (res) => {
+        this.messageError(false);
+        this.saveUser(res as User);
+        console.log(res);
+      },
+      error: (err) => this.messageError(true)
+    });
+  }
+  
 }
