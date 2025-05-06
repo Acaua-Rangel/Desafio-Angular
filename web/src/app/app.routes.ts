aba345cc19e2d70b9d0ecefc44c63a4e77106da1
AcauRangel <acauarangel17@gmail.com>
2025-05-06 11:31:27 -0300

    Angular Router

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './page/login-page/login-page.component';
+import { WelcomePageComponent } from './page/welcome-page/welcome-page.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: WelcomePageComponent
+    },
     {
         path: 'login',
         component: LoginPageComponent
---
+++ web/src/app/page/welcome-page/welcome-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-welcome-page',
+  imports: [HeaderComponent],
+  templateUrl: './welcome-page.component.html',
+  styleUrl: './welcome-page.component.css'
+})
+export class WelcomePageComponent {
+
+}
